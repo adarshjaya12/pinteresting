6557c09ceb19fdd25b1087e765ea3be1b8895fc2
Adarsh Jayakumar <adarshjaya12@gmail.com>
2016-08-14 19:49:19 -0500

    Included image_file_name accessor in pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+	attr_accessor :image_file_name
 	has_attached_file :image
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
